051286683513e7ebbedf06cd3d2269bfaf5f7243
Pedro Sampaio <pedroigorjs@gmail.com>
2020-07-08 06:35:00 -0300

    Create api service

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import logo from '../logo.svg';
 import Navbar from '../components/Navbar';
+import api from '../services/api';
 import { Container } from './Home/styles';
 
 export default function Home() {
