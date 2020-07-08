5c513f77c1cdd35c94f3ed2166c65802be12d949
Pedro Sampaio <pedroigorjs@gmail.com>
2020-07-08 19:36:23 -0300

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -50,11 +50,13 @@ export default function Home() {
   useEffect(() => {
     getPokemon();
 
+    /*
     function handleSearch() {
       if (searchValue.length > 0) {
         const newPokemon = pokemon.filter((item) => item.name.toLowerCase().includes(searchValue.toLowerCase()));
         console.log(newPokemon);
       }
+     */
     }
 
     // handleSearch();
