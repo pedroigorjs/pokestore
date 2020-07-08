29af0b956befa7083c46adcc3197b0bd992e7f97
Pedro Sampaio <pedroigorjs@gmail.com>
2020-07-08 06:33:25 -0300

    Create Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Home from './pages/Home';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <>
+      <Home />
       <GlobalStyles />
     </>
   );
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+import { Container } from './Home/styles';
+
+export default function Home() {
+  return (
+    <>
+    </>
+  );
+}
